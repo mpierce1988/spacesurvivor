65494a6172d9117d27c93fd30683b719a08459b4
mpierce1988 <mpierce1138@gmail.com>
2022-08-30 11:44:27 -0300

    Basic player movement and rotation complete

--- Assets/Scripts/Master Singleton/MasterSingleton.cs
+++ Assets/Scripts/Master Singleton/MasterSingleton.cs
@@ -9,8 +9,28 @@ public class MasterSingleton : MonoBehaviour
     private SceneLoader _sceneLoader;
     private InputController _inputController;
 
-    public SceneLoader SceneLoader => _sceneLoader;
-    public InputController InputController { get { return _inputController; } }
+    public SceneLoader SceneLoader 
+    { 
+        get 
+        { 
+            if(_sceneLoader == null)
+            {
+                _sceneLoader = GetComponentInChildren<SceneLoader>();
+            }
+            return _sceneLoader; 
+        } 
+    }
+    public InputController InputController 
+    { 
+        get 
+        {
+            if(_inputController == null)
+            {
+                _inputController = GetComponentInChildren<InputController>();
+            }
+            return _inputController; 
+        } 
+    }
 
     
 
@@ -18,7 +38,13 @@ public class MasterSingleton : MonoBehaviour
     {
         CreateSingleton();
 
-        _sceneLoader = GetComponentInChildren<SceneLoader>();
+        if(_sceneLoader == null)
+            _sceneLoader = GetComponentInChildren<SceneLoader>();
+        
+        if(_inputController == null)
+        {
+            _inputController = GetComponentInChildren<InputController>();
+        }
     }
 
     private void CreateSingleton()
